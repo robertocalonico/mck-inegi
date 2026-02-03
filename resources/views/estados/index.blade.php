19e7c70ef038e1c60cc5b17f4e061513a0bf23f3
Roberto Calonico <isc.calonico@gmail.com>
2026-02-03 14:31:27 -0600

    Mejora responsive en vistas de estados

--- resources/views/estados/index.blade.php
+++ resources/views/estados/index.blade.php
@@ -9,34 +9,36 @@
 <body class="p-4">
 <div class="container">
     <h1 class="mb-4">Estados</h1>
-    <table id="tabla-estados" class="table table-striped">
-        <thead>
-            <tr>
-                <th>CVEGEO</th>
-                <th>CVE_AGEE</th>
-                <th>NOMBRE</th>
-                <th>ABREVIATURA</th>
-                <th>POB</th>
-                <th>ACCIONES</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($estados as $estado)
-            <tr>
-                <td>{{ $estado->cvegeo }}</td>
-                <td>{{ $estado->cve_agee }}</td>
-                <td>{{ $estado->nom_agee }}</td>
-                <td>{{ $estado->nom_abrev }}</td>
-                <td>{{ number_format($estado->pob) }}</td>
-                <td>
-                    <a class="btn btn-sm btn-primary" href="{{ route('estados.show', $estado) }}">
-                        Ver
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table id="tabla-estados" class="table table-striped align-middle">
+            <thead>
+                <tr>
+                    <th>CVEGEO</th>
+                    <th>CVE_AGEE</th>
+                    <th>NOMBRE</th>
+                    <th>ABREVIATURA</th>
+                    <th>POB</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($estados as $estado)
+                <tr>
+                    <td>{{ $estado->cvegeo }}</td>
+                    <td>{{ $estado->cve_agee }}</td>
+                    <td>{{ $estado->nom_agee }}</td>
+                    <td>{{ $estado->nom_abrev }}</td>
+                    <td>{{ number_format($estado->pob) }}</td>
+                    <td>
+                        <a class="btn btn-sm btn-primary w-100" href="{{ route('estados.show', $estado) }}">
+                            Ver
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
--- resources/views/estados/show.blade.php
+++ resources/views/estados/show.blade.php
@@ -9,30 +9,34 @@
 <div class="container">
     <h1 class="mb-4">Detalle de {{ $estado->nom_agee }}</h1>
 
-    <form>
-        <div class="mb-3">
-            <label class="form-label">CVEGEO</label>
-            <input class="form-control" value="{{ $estado->cvegeo }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">CVE_AGEE</label>
-            <input class="form-control" value="{{ $estado->cve_agee }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">NOMBRE</label>
-            <input class="form-control" value="{{ $estado->nom_agee }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">ABREVIATURA</label>
-            <input class="form-control" value="{{ $estado->nom_abrev }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">POBLACIÓN</label>
-            <input class="form-control" value="{{ number_format($estado->pob) }}" readonly>
-        </div>
+    <div class="row">
+        <div class="col-12 col-md-8 col-lg-6">
+            <form>
+                <div class="mb-3">
+                    <label class="form-label">CVEGEO</label>
+                    <input class="form-control" value="{{ $estado->cvegeo }}" readonly>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">CVE_AGEE</label>
+                    <input class="form-control" value="{{ $estado->cve_agee }}" readonly>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">NOMBRE</label>
+                    <input class="form-control" value="{{ $estado->nom_agee }}" readonly>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">ABREVIATURA</label>
+                    <input class="form-control" value="{{ $estado->nom_abrev }}" readonly>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">POBLACION</label>
+                    <input class="form-control" value="{{ number_format($estado->pob) }}" readonly>
+                </div>
 
-        <a href="{{ route('estados.index') }}" class="btn btn-secondary">Volver</a>
-    </form>
+                <a href="{{ route('estados.index') }}" class="btn btn-secondary w-100">Volver</a>
+            </form>
+        </div>
+    </div>
 </div>
 </body>
 </html>
